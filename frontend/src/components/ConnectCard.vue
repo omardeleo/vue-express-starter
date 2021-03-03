3484eef0cba4e60e3185efff3ac6075b888d56d6
Omar De Leo <omar.deleo@gmail.com>
2021-03-03 18:11:51 -0500

    Connect to server

--- frontend/src/components/ConnectCard.vue
+++ frontend/src/components/ConnectCard.vue
@@ -4,15 +4,23 @@
       <h2>Connect to a server</h2>
       <h3>Powered by [Backend logo]</h3>
       <p>
-        This Vue frontend is connected to an [xxxxxx] server. Below is the
+        This Vue frontend is connected to an Express server. Below is the
         response message we receive when we ping the server:
       </p>
-      <p>[response]</p>
+      <p>{{ data.response }}</p>
       <p>
         The server ping count is stored to the database. Click below to reset
         the counter:
       </p>
-      <p>[reset button]</p>
+      <v-btn
+        depressed
+        elevation="2"
+        color="primary"
+        outlined
+        @click="resetCounter"
+      >
+        RESET COUNTER
+      </v-btn>
       <p>
         The endpoint that resets the ping counter is located in
         [backend/src/routes/index.js]:
@@ -25,5 +33,34 @@
 <script>
 export default {
   name: 'ConnectCard',
+  data() {
+    return {
+      data: {
+        response: '',
+      },
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      fetch('/api/v1/')
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            this.data.response = result.response;
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    },
+    async resetCounter() {
+      fetch('/api/v1/reset/')
+        .then((res) => res.json())
+        .then(() => this.fetchData());
+    },
+  },
 };
 </script>
