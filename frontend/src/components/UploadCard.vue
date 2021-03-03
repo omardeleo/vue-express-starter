61b219034841c378cf9d8367d4bba8be4b2f39a7
Omar De Leo <omar.deleo@gmail.com>
2021-03-03 18:16:03 -0500

    Upload files to s3

--- frontend/src/components/UploadCard.vue
+++ frontend/src/components/UploadCard.vue
@@ -9,8 +9,34 @@
         <b>LocalStack</b>
         - a fully functional local AWS cloud stack, and displayed below.
       </p>
-      <p>[button]</p>
-      <p>[thumbnail grid]</p>
+      <div>
+        <v-btn
+          depressed
+          elevation="2"
+          color="primary"
+          outlined
+          @click="clickHandler"
+        >
+          UPLOAD IMAGE
+        </v-btn>
+        <input
+          ref="uploader"
+          class="d-none"
+          type="file"
+          accept="image/*"
+          @change="submitFile"
+        />
+      </div>
+      <v-row>
+        <v-col
+          v-for="filename in data.filenames"
+          :key="filename"
+          class="d-flex child-flex"
+          cols="4"
+        >
+          <v-img :src="filename"></v-img>
+        </v-col>
+      </v-row>
     </v-card>
   </v-col>
 </template>
@@ -18,5 +44,57 @@
 <script>
 export default {
   name: 'UploadCard',
+  data() {
+    return {
+      data: {
+        filenames: [],
+      },
+    };
+  },
+  mounted() {
+    this.fetchBucketFiles();
+  },
+  methods: {
+    clickHandler() {
+      this.$refs.uploader.click();
+    },
+    async fetchBucketFiles() {
+      fetch('api/v1/files')
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            const bucket = result['Name'];
+            const filenames = result['Contents']
+              ? result['Contents']
+                  .map((file) => `${bucket}/${file['Key']}`)
+                  .reverse()
+              : [];
+            console.log('filenames', filenames);
+            this.data.filenames = filenames;
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    },
+    async submitFile(e) {
+      e.preventDefault();
+      const file = e.target.files;
+      try {
+        if (!file) {
+          throw new Error('Please select an image');
+        }
+        const formData = new FormData();
+        formData.append('file', file[0]);
+        await fetch('/api/v1/files/upload/', {
+          method: 'POST',
+          body: formData,
+        });
+        this.fetchBucketFiles();
+      } catch (error) {
+        alert(error);
+      }
+    },
+  },
 };
 </script>
